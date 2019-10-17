3f343cd6a0f8c1ea1d1c7dcaec472c2b4b1fec00
simonericci <simogol@gmail.com>
2019-10-17 19:32:22 +0200

    no message

--- backoffice/index.php
+++ backoffice/index.php
@@ -36,7 +36,6 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
     <title>Safe Landings Demo</title>
     <style>
       .moreinfo { border-bottom: 1px dotted #000; text-decoration: none;}
@@ -123,8 +122,8 @@
                                       <td><?php echo $row['id']?></td>
                                       <td><?php echo $row['first_name'];?></td>
                                       <td><?php echo $row['last_name']?></td>
-                                      <td><span class="moreinfo" title="string actually stored in database: <?php echo $row['email'];?>"><?php echo $dao->dec($row['email']);?></span></td>
-                                      <td><span class="moreinfo" title="string actually stored in database: <?php echo $row['email'];?>"><?php echo $dao->dec($row['phone']);?></span></td>
+                                      <td><span class="moreinfo" data-toggle="tooltip" title="string actually stored in database: <?php echo $row['email'];?>"><?php echo $dao->dec($row['email']);?></span></td>
+                                      <td><span class="moreinfo" data-toggle="tooltip" title="string actually stored in database: <?php echo $row['email'];?>"><?php echo $dao->dec($row['phone']);?></span></td>
                                       <td><?php echo $row['creation_date']?></td>
                                       <td><?php echo $row['source']?></td>                           
                                       <td><?php echo $row['privacy']?></td>
@@ -157,5 +156,10 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script>
+    $(document).ready(function(){
+      $('[data-toggle="tooltip"]').tooltip();
+    });
+    </script>
   </body>
 </html>
